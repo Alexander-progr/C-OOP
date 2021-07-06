60f27931558f7f894369bc8956e5487721b509c3
Alexander-progr <64677170+Alexander-progr@users.noreply.github.com>
2021-07-06 17:45:28 +0500

    Add files via upload

---
+++ LabRab6/LabRab6/Models/IFigure.cs
@@ -0,0 +1,10 @@
+﻿namespace LabRab6.Models
+{
+    public interface IFigure
+    {
+        string Name { get; set; }
+        string Pos { get; set; }
+        bool CheckPos(string pos);
+    }
+}
+
